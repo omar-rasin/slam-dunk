7e49dd5fe61d7a06dd8fc3a7ef551a2b5f8c8d29
omar-rasin <omarasinyt@gmail.com>
2024-08-14 22:31:25 +0600

    Main Function along with the Main-Menu interface created

--- main.c
+++ main.c
@@ -4,3 +4,18 @@
 #include<string.h>
 #include<float.h>
 
+int main() {
+
+    printf("\n");
+    
+    printf("WELCOME TO SLAM-DUNK BASKETBALL CLUB!\n\n"); 
+    
+    printf("MAIN MENU:\n");
+
+    int choice;
+    printf("1.Team Roster\n2.Player Information\n3.Player Stats\n4.Coaches Roster\n5.Equipment Inventory\n6.Financial Records\n\n");
+    printf("Please Enter the number of your choice: ");
+    scanf("%d", &choice);
+
+    return 0;
+}
