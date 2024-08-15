bb3f6152df134ec9272a31d66eb542497b641200
omar-rasin <omarasinyt@gmail.com>
2024-08-15 22:18:49 +0600

    Secondary interface created which allows user to choose which 'CRUD' operation to perform

--- main.c
+++ main.c
@@ -17,5 +17,20 @@ int main() {
     printf("Please Enter the number of your choice: ");
     scanf("%d", &choice);
 
+    if(choice>=1 && choice<=6 ) {
+
+        printf("\n");
+        printf("What Operation would you like to perform?\n\n");
+        printf("1.View Records\n2.Add and Create new Records\n3.Delete Existing Records\n4.Search records based off criteria\n\n");
+
+        int preference;
+        printf("Enter the number of your preference: ");
+        scanf("%d", &preference);
+
+    }else {
+        
+        printf("Invalid number of choice inputted - Please try again");
+    }
+
     return 0;
 }
